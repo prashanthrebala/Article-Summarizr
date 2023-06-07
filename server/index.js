11696a6c47b390b8e163ed8fa6be22471d105946
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-07 19:35:10 -0400

    created dummy data for testing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
+const { dummyData } = require("./dummyData");
 
 require("dotenv").config();
 
