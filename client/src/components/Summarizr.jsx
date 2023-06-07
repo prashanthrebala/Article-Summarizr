4e2538e95bc3bf08865ab2ecb3bc4d3c57dd4d97
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-07 16:48:51 -0400

    basic layout complete

--- client/src/components/Summarizr.jsx
+++ client/src/components/Summarizr.jsx
@@ -1,8 +1,11 @@
 import { useRef, useState } from "react";
+import background from "../assets/haikei-blob.png";
 
 export const Summarizr = () => {
 	const urlInputRef = useRef();
-	const [summarizedData, setSummarizedData] = useState("");
+	const [summarizedData, setSummarizedData] = useState(
+		"<p>Your summarized article will appear here...<p>"
+	);
 	const handleKeyPress = async (event) => {
 		if (event.key === "Enter") {
 			// const url = urlInputRef.current.value;
@@ -15,7 +18,6 @@ export const Summarizr = () => {
 				});
 				const data = await response.json();
 				console.log("API response:", data);
-				// response.summary.replace(/\n/g, "<br>")
 				setSummarizedData(data.summary.replace(/\n/g, "<br>"));
 			} catch (error) {
 				console.error("API error:", error);
@@ -24,20 +26,42 @@ export const Summarizr = () => {
 	};
 
 	return (
-		<div className="flex items-center md:justify-center justify-around md:flex-row flex-col min-h-screen">
-			<div className="w-full md:w-1/2 flex items-center justify-center flex-col space-y-6">
-				<div className="text-5xl">Text Summarizr</div>
+		<div
+			className="flex items-center md:justify-center md:flex-row flex-col min-h-screen text-neutral-100"
+			style={{
+				background: `url(${background})`,
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+			}}
+		>
+			<div className="w-full md:w-1/2 flex items-center justify-center flex-col space-y-6 my-6">
+				<div
+					style={{
+						fontFamily: "Passion One",
+						fontWeight: "700",
+						fontSize: "4em",
+					}}
+					className="text-5xl w-5/6 text-center"
+				>
+					Article Summarizr
+				</div>
 				<input
 					ref={urlInputRef}
 					onKeyDown={handleKeyPress}
-					className="w-5/6 rounded-lg p-2 outline-0 h-10 border-2 border-amber-500 bg-slate-100"
+					className="w-5/6 rounded-lg p-2 outline-0 h-14 border-2 border-green-600 bg-slate-100 text-neutral-800"
 					placeholder="Enter a URL here"
 				/>
+				<div className="w-5/6 text-sm text-center">
+					{`This is a website that uses a RapidAPI that extracts news/article body from a URL and uses GPT to summarize the article content.`}
+				</div>
 			</div>
-			<div className="w-full md:w-1/2 text-center md:min-h-screen bg-amber-500 flex justify-center md:py-24">
+			<div className="w-full md:w-1/2 text-center h-full md:min-h-screen flex justify-center md:items-center py-4 md:py-16">
 				<div
-					className="bg-red-500 w-5/6 text-justify"
-					dangerouslySetInnerHTML={{ __html: summarizedData }}
+					className="w-11/12 md:w-5/6 text-justify md:overflow-y-auto md:h-[80vh] bg-green-900 rounded-md p-2 md:p-8"
+					dangerouslySetInnerHTML={{
+						__html: summarizedData,
+					}}
 				></div>
 			</div>
 		</div>
