76885e490725ce96d94dcb4d3decd0de84336f4a
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-08 15:18:08 -0400

    logs + error handling

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const { dummyData } = require("./dummyData");
 
 require("dotenv").config();
 
+const DEBUG_MODE = process.env.ENVIRONMENT === "development";
 const PORT_NUMBER = process.env.SERVER_PORT || 9000;
 
 app.use(
@@ -15,10 +16,9 @@ app.use(
 );
 
 app.get("/summarize", async (req, res) => {
-	console.log("we have a request errybody!");
 	const articleLink = req.query.articleLink;
 	const numberOfParagraphs = req.query.numberOfParagraphs || 3;
-	console.log(articleLink);
+	console.log(`request received for ${articleLink}`);
 
 	const options = {
 		method: "GET",
@@ -34,14 +34,26 @@ app.get("/summarize", async (req, res) => {
 	};
 
 	try {
-		const response = await axios.request(options);
+		const response = DEBUG_MODE ? dummyData : await axios.request(options);
+		console.log("api fetch succeeded");
 		res.json(response.data);
 	} catch (error) {
 		console.error(error);
-		res.sendStatus(503);
+		if (error.response) {
+			res.status(error.response.status).json({
+				error: error.response.data,
+			});
+		} else if (error.request) {
+			res.sendStatus(503);
+		} else {
+			res.status(500).json({
+				error: "Internal Server Error",
+			});
+		}
 	}
 });
 
 app.listen(PORT_NUMBER, () => {
+	console.log(`Starting Summarizr server in ${process.env.ENVIRONMENT} mode`);
 	console.log(`listening on port ${PORT_NUMBER}`);
 });
