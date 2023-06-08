fcf26e56e765015e393e4a4db6afc337ed2466cc
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-08 16:25:01 -0400

    improved UI

--- client/src/components/Summarizr.jsx
+++ client/src/components/Summarizr.jsx
@@ -3,7 +3,8 @@ import { SUMMARIZR_BASE_URL } from "../constants/constants";
 import { MdError } from "react-icons/md";
 import { TripleMaze } from "react-spinner-animated";
 import axios from "axios";
-import background from "../assets/haikei-blob.png";
+// import background from "../assets/haikei-blob.png";
+import background from "../assets/summarizr-bg.png";
 import "react-spinner-animated/dist/index.css";
 
 export const Summarizr = () => {
@@ -39,39 +40,39 @@ export const Summarizr = () => {
 	};
 
 	return (
-		<div
-			className="flex items-center md:justify-center md:flex-row flex-col min-h-screen text-neutral-100"
-			style={{
-				background: `url(${background})`,
-				backgroundRepeat: "no-repeat",
-				backgroundSize: "cover",
-				backgroundPosition: "center",
-			}}
-		>
-			<div className="w-full md:w-1/2 flex items-center justify-center flex-col space-y-6 my-6">
+		<div className="flex items-center md:justify-center md:flex-row flex-col min-h-screen text-neutral-100 bg-gray-900">
+			<div
+				className="w-full md:w-1/2 flex items-center justify-center flex-col space-y-6 py-6 md:min-h-screen"
+				style={{
+					background: `url(${background})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			>
 				<div
 					style={{
 						fontFamily: "Passion One",
 						fontWeight: "700",
 						fontSize: "4em",
 					}}
-					className="text-5xl w-5/6 text-center"
+					className="text-5xl w-5/6 text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.9)]"
 				>
 					Article Summarizr
 				</div>
 				<input
 					ref={urlInputRef}
 					onKeyDown={handleKeyPress}
-					className="w-5/6 rounded-lg p-2 outline-0 h-14 border-2 border-green-600 bg-slate-100 text-neutral-800"
+					className="w-5/6 rounded-lg p-2 outline-0 h-14 border-2 border-neutral-600 bg-slate-100 text-neutral-800"
 					placeholder="Enter a URL here"
 				/>
 				<div className="w-5/6 text-sm text-center">
 					{`This is a website that uses a RapidAPI that extracts news/article body from a URL and uses GPT to summarize the article content.`}
 				</div>
 			</div>
-			<div className="w-full md:w-1/2 text-center h-full md:min-h-screen flex justify-center items-center py-4 md:py-16">
+			<div className="w-full md:w-1/2 text-center text-neutral-950 h-full md:min-h-screen flex justify-center items-center py-4 md:py-16">
 				<div
-					className={`w-11/12 md:w-5/6 text-justify md:overflow-y-auto md:h-[80vh] bg-green-900 rounded-md p-2 md:p-8 ${
+					className={`w-11/12 md:w-5/6 text-justify md:overflow-y-auto md:h-[80vh] bg-gray-300 rounded-md p-2 md:p-8 ${
 						isLoading || errorMessage ? "flex items-center justify-center" : ""
 					}`}
 				>
@@ -90,7 +91,10 @@ export const Summarizr = () => {
 						</div>
 					)}
 					{!isLoading && !errorMessage && (
-						<div dangerouslySetInnerHTML={{ __html: summarizedData }} />
+						<div
+							className="p-4"
+							dangerouslySetInnerHTML={{ __html: summarizedData }}
+						/>
 					)}
 				</div>
 			</div>
