b6819e12445b41371bff989e239c7233bc806cd9
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-16 22:28:30 -0400

    created api folder for requests

---
+++ client/src/api/summarizrApi.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { SUMMARIZR_BASE_URL } from "../config";
+
+export const fetchSummarizedData = async (articleLink) => {
+	try {
+		const response = await axios.get(`${SUMMARIZR_BASE_URL}/summarize`, {
+			params: {
+				articleLink,
+			},
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = response.data;
+		return data.summary.replace(/\n/g, "<br>");
+	} catch (error) {
+		console.error("API error:", error);
+		throw new Error("There was an error fetching your article");
+	}
+};
