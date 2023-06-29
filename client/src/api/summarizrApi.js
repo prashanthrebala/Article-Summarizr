e69d66d769102f6e255a10b2559b8f631bea073d
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-29 18:22:06 -0400

    handle too many requests error on client side

--- client/src/api/summarizrApi.js
+++ client/src/api/summarizrApi.js
@@ -15,6 +15,9 @@ export const fetchSummarizedData = async (articleLink) => {
 		return data.summary.replace(/\n/g, "<br>");
 	} catch (error) {
 		console.error("API error:", error);
+		if (error.response?.status === 429) {
+			throw new Error(error.response.data.error);
+		}
 		throw new Error("There was an error fetching your article");
 	}
 };
