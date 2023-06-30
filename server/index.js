41c485e3be3e447fa4c1d99435751043eacea2dd
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-06-30 16:15:19 -0400

    set up redis to work within docker

--- server/index.js
+++ server/index.js
@@ -14,12 +14,29 @@ const {
 	RAPID_API_HOST,
 	SERVER_API_KEY,
 	SERVER_PORT,
+	REDIS_HOST,
+	REDIS_PORT,
 } = require("./config.js");
 
 const app = express();
-const redisClient = createClient();
+const redisClient = createClient({
+	socket: {
+		host: REDIS_HOST,
+		port: REDIS_PORT,
+	},
+});
 const { dummyData } = require("./dummyData");
 const summariesRouter = require("./routes/summaries.js");
+
+const getTimeTillEOD = function () {
+	const now = new Date();
+	const endOfDay = new Date(
+		now.getFullYear(),
+		now.getMonth(),
+		now.getDate() + 1
+	);
+	return Math.floor((endOfDay - now) / 1000);
+};
 
 app.use("/summaries", summariesRouter);
 app.use(cors({ origin: ALLOWED_ORIGIN }));
@@ -35,17 +52,26 @@ app.use(async (req, res, next) => {
 
 app.use(async (req, res, next) => {
 	try {
-		const value = await redisClient.get(req.socket.remoteAddress);
-		const count = value !== null ? parseInt(value) + 1 : 1;
-		redisClient.set(req.socket.remoteAddress, count);
-		if (count >= 3) {
+		const clientIP = req.socket.remoteAddress;
+		const countOfTheDay = "countOfTheDay";
+		const value = await redisClient.get(clientIP);
+		const today = await redisClient.get(countOfTheDay);
+		const count = value !== null ? parseInt(value) : 0;
+		const todayCount = today !== null ? parseInt(today) : 0;
+		if (count >= 2 || todayCount >= 3) {
 			return res.status(429).json({
 				error:
 					"Too Many Requests. Since this is a free version, \
-				this API is limited to 2 requests per IP address and \
-				3 requests per day overall. Please try again tomorrow!",
+					this API is limited to 2 requests per IP address and \
+					3 requests per day overall. Please try again tomorrow!",
 			});
 		}
+		redisClient.setEx(clientIP, getTimeTillEOD(), (count + 1).toString());
+		redisClient.setEx(
+			countOfTheDay,
+			getTimeTillEOD(),
+			(todayCount + 1).toString()
+		);
 		next();
 	} catch (error) {
 		console.error("Error checking key existence in Redis:", error);
