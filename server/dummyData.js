31f15eb2fb0e0725d8fbbdb8de17ca3e843bbf8c
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-07-01 11:49:58 -0400

    removed dummyData from gitignore

---
+++ server/dummyData.js
@@ -0,0 +1,12 @@
+const dummyData = {
+	data: {
+		summary:
+			"NASA and SpaceX could collaborate on a mission to Mars, combining the space agency's nuclear propulsion technology with the SpaceX Starship. The Starship could move 180 metric tons to the Martian surface, and with a nuclear engine, it could do so in a matter of weeks rather than months. A joint mission would also benefit from SpaceX's cost-cutting and outside-the-box thinking and NASA's institutional experience and resources. \n" +
+			"\n" +
+			"However, the engineering task of integrating a nuclear component into the SpaceX Starship would be formidable, and the two entities would need to reconcile their differing motivations for going to Mars. Musk's desire to extend life to other planets and NASA's mission to look for life out there will need to be compatible. A permanent colony on Mars would change the nature of the planet, and reconciling scientists' desire to keep Mars pristine with Musk's ambition to establish a new branch of civilization there would be a tough undertaking. \n" +
+			"\n" +
+			"A joint mission to Mars could happen in the early to mid-2030s, with the run-up to getting humans to Mars taking place simultaneously with other space priorities, such as the return to the moon and the development of commercial space stations to replace the ISS. Sufficient resources from NASA and its international and commercial partners must be made available.",
+	},
+};
+
+module.exports = { dummyData };
